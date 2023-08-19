e40a9f1eff5e35a6c518309d89d0d78315502cad
vansh909 <133160909+vansh909@users.noreply.github.com>
2023-08-19 17:10:32 +0530

    bllinkName

--- BlinkName.ino
+++ BlinkName.ino
@@ -1,103 +1,114 @@
 const int led = 13; // Led Pin Number
+const int button=4;
+bool buttonstatus=false;
+bool buttonlaststatus=false;
 
 void setup() {
   pinMode(led, OUTPUT);    // Setting the pin as OUTPUT pin
+  pinMode(button, INPUT);
 }
 
 void loop() {
-  char msg[] = "VANSH"; // HERE WE ARE STORING THE NAME IN ARRAY
-  int msglength = strlen(msg);
-  
-  for (int i = 0; i < msglength; i++) {     //THE LOOP WILL RUN THROUGHOUT THE LENGTH OF AN ARRAY
-    char a = toupper(msg[i]);         
-
-    switch (a) {
-      case 'A':
-        blinkFunc(".-");
-        break;
-      case 'B':
-        blinkFunc("-...");
-        break;
-      case 'C':
-        blinkFunc("-.-.");
-      case 'D':
-        blinkFunc("-..");
-        break;
-      case 'E':
-        blinkFunc(".");
-        break;
-      case 'F':
-        blinkFunc("-.");
-        break;
-      case 'G':
-        blinkFunc("--.");
-        break;
-       case 'H':
-        blinkFunc("....");
-        break;
-       case 'I':
-        blinkFunc("..");
-        break;
-       case 'J':
-        blinkFunc(".---");
-        break;
-       case 'K':
-        blinkFunc("-.-");
-        break;
-       case 'L':
-        blinkFunc(".-..");
-        break;
-       case 'M':
-        blinkFunc("--");
-        break;
-       case 'N':
-        blinkFunc("-.");
-        break;
-       case 'O':
-        blinkFunc("---");
-        break;
-       case 'P':
-        blinkFunc(".--.");
-        break;
-       case 'Q':
-        blinkFunc("--.-");
-        break;
-       case 'R':
-        blinkFunc(".-.");
-        break;
-       case 'S':
-        blinkFunc("...");
-        break;
-       case 'T':
-        blinkFunc("-");
-        break;
-       case 'U':
-        blinkFunc("..-");
-        break;
-       case 'V':
-        blinkFunc("...-");
-        break;
-       case 'W':
-        blinkFunc(".--");
-        break;
-       case 'X':
-        blinkFunc("-..-");
-        break;
-       case 'Y':
-        blinkFunc("-.--");
-        break;
-       case 'Z':
-        blinkFunc("--..");
-        break;
-      default:
-        delay(300);
-        break;
-    }
-    delay(3000); // SPACING BETWEEN THE LETTERS
+  buttonstatus=digitalRead(button);
+  if (buttonstatus != buttonlaststatus){
+    if(buttonstatus==HIGH){
+      char msg[] = "VANSH"; // HERE WE ARE STORING THE NAME IN ARRAY
+      int msglength = strlen(msg);
+    
+      for (int i = 0; i < msglength; i++) {     //THE LOOP WILL RUN THROUGHOUT THE LENGTH OF AN ARRAY
+        char a = toupper(msg[i]);         
+    
+        switch (a) {
+          case 'A':
+            blinkFunc(".-");
+            break;
+          case 'B':
+            blinkFunc("-...");
+            break;
+          case 'C':
+            blinkFunc("-.-.");
+          case 'D':
+            blinkFunc("-..");
+            break;
+          case 'E':
+            blinkFunc(".");
+            break;
+          case 'F':
+            blinkFunc("-.");
+            break;
+          case 'G':
+            blinkFunc("--.");
+            break;
+           case 'H':
+            blinkFunc("....");
+            break;
+           case 'I':
+            blinkFunc("..");
+            break;
+           case 'J':
+            blinkFunc(".---");
+            break;
+           case 'K':
+            blinkFunc("-.-");
+            break;
+           case 'L':
+            blinkFunc(".-..");
+            break;
+           case 'M':
+            blinkFunc("--");
+            break;
+           case 'N':
+            blinkFunc("-.");
+            break;
+           case 'O':
+            blinkFunc("---");
+            break;
+           case 'P':
+            blinkFunc(".--.");
+            break;
+           case 'Q':
+            blinkFunc("--.-");
+            break;
+           case 'R':
+            blinkFunc(".-.");
+            break;
+           case 'S':
+            blinkFunc("...");
+            break;
+           case 'T':
+            blinkFunc("-");
+            break;
+           case 'U':
+            blinkFunc("..-");
+            break;
+           case 'V':
+            blinkFunc("...-");
+            break;
+           case 'W':
+            blinkFunc(".--");
+            break;
+           case 'X':
+            blinkFunc("-..-");
+            break;
+           case 'Y':
+            blinkFunc("-.--");
+            break;
+           case 'Z':
+            blinkFunc("--..");
+            break;
+          default:
+            delay(300);
+            break;
+        }
+    
+        delay(3000); // SPACING BETWEEN THE LETTERS
+      }
+      delay(1000); 
+      }
+      buttonlaststatus = buttonstatus;
   }
-  delay(1000); 
 }
-// Implementing The Function For Blinking
+
 void blinkFunc(const char* morsecode) {
   for (int i = 0; morsecode[i]; i++) {    // For Loop For Running Through the Morsecode
     if (morsecode[i] == '.') {            // For Running The dot Method
